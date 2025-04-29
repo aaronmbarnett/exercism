ed2af8c89d49d38e0bc5bb5fe5e23e26369f0a09
Aaron Barnett <aaronbarnett@Aarons-Mac-mini.local>
2025-04-29 17:31:42 -0500

    Completed Rust - Reverse Strings

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,6 +1,12 @@
 pub fn reverse(input: &str) -> String {
-    let result = String::new();
-    for c in input.chars() {
-        result.push_str(c);
+    let mut result = String::new();
+    let char_array: Vec<char> = input.chars().collect();
+    let mut stack = vec![];
+    for c in char_array {
+        stack.push(c);
     }
+    for c in stack {
+        result.insert(0, c);
+    }
+    result
 }
